5d2e119c1d804dbdde316507b3565150f60e8ff6
Jordan Hall <jordan.hall@rapidweb.biz>
2018-01-26 13:53:50 +0000

    Laravel cache driver test

---
+++ tests/Unit/LaravelCacheDriverTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use RapidWeb\UkBankHolidays\Factories\UkBankHolidayFactory;
+
+final class LaravelCacheDriverTest extends TestCase
+{
+    private function checkHolidaysArrayFormat($holidays)
+    {
+        $this->assertTrue(is_array($holidays), 'Holidays should be an array.');
+        $this->assertTrue(count($holidays) > 0, 'Holidays array should have at least 1 value.');
+
+        $holiday = $holidays[0];
+
+        $this->assertEquals('RapidWeb\UkBankHolidays\Objects\UkBankHoliday', get_class($holiday));
+    }
+
+    public function testEnglandWales()
+    {
+        require_once __DIR__.'/includes/MockLaravelCache.php';
+        $this->checkHolidaysArrayFormat(UkBankHolidayFactory::getAll('england-and-wales'));
+    }
+
+}
---
+++ tests/Unit/includes/MockLaravelCache.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Illuminate\Support\Facades;
+
+abstract class Cache
+{
+    public static function put($key, $value, $expiry) {
+
+    }
+
+    public static function get($key) {
+        
+    }
+}
