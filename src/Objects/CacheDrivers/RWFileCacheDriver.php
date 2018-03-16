17080daa7a95013e1b1e241f3333f3e5412b1fc0
Jordan Hall <jordan.hall@rapidweb.biz>
2018-03-16 17:27:23 +0000

    Change RWFileCacheDriver to use RWFileCachePSR6 CacheItemPool

--- src/Objects/CacheDrivers/RWFileCacheDriver.php
+++ src/Objects/CacheDrivers/RWFileCacheDriver.php
@@ -4,14 +4,17 @@ namespace RapidWeb\UkBankHolidays\Objects\CacheDrivers;
 
 use rapidweb\RWFileCache\RWFileCache;
 use RapidWeb\UkBankHolidays\Interfaces\CacheDriverInterface;
+use rapidweb\RWFileCachePSR6\CacheItemPool;
 
 class RWFileCacheDriver implements CacheDriverInterface
 {
+    const CACHE_EXPIRY_IN_SECONDS = 60*60*24*30; // 30 days
+
     private $cache = null;
 
     public function __construct()
     {
-        $this->cache = new RWFileCache();
+        $this->cache = new CacheItemPool();
         $this->cache->changeConfig(
             [
                 'cacheDirectory'  => '/tmp/php-uk-bank-holidays-cache/',
@@ -22,11 +25,13 @@ class RWFileCacheDriver implements CacheDriverInterface
 
     public function set($key, $value)
     {
-        return $this->cache->set($key, $value, strtotime('+1 month'));
+        $cacheItem = $this->cache->getItem($key);
+        $cacheItem->set($value);
+        $cacheItem->expiresAfter(self::CACHE_EXPIRY_IN_SECONDS);
     }
 
     public function get($key)
     {
-        return $this->cache->get($key);
+        return $this->cache->getItem($key)->get();
     }
 }
